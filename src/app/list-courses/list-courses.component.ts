87e0089a09b3d58ffb18eab24a3d871025e1def6
mehdi Elaou <mehdidevinformatique@gmail.com>
2019-11-27 12:17:00 +0100

    mehdi

--- src/app/list-courses/list-courses.component.ts
+++ src/app/list-courses/list-courses.component.ts
@@ -6,23 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-courses.component.css']
 })
 export class ListCoursesComponent {
-image="https://picsum.photos/id/400/800/300";
 
-<<<<<<< HEAD
-    myCourse: string = 'Learn Angular';
-    
-    courses: string[] = ['Angular', 'Spring', 'Laravel'];
-    
-    num1: number = 0;
-    num2: number = 0;
-    addCourse(){
-      // this.courses.unshift(this.myCourse);
-      this.courses=[this.myCourse,...this.courses];
-      this.myCourse="";
-      }
-    
-    }
-=======
   myCourse: string = 'Learn Angular';
   image = "https://picsum.photos/id/237/535/200"
   courses: string[] = ['Angular', 'Spring', 'Laravel'];
@@ -38,6 +22,5 @@ image="https://picsum.photos/id/400/800/300";
     this.myCourse= course;
     console.log(this.myCourse);
   }
->>>>>>> f760d795612455157ed4ad3f626ca68ea3600d91
 
-
+}
